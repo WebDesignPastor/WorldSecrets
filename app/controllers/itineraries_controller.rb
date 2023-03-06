37880cfc7e7e4c2b470885bc8b1908fc27bfad27
Francois PARMENTIER$ <parmentier.corporate@gmail.com>
2023-03-06 19:55:35 +0100

    Views itineraries index show + pages home + pois show

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,9 +1,10 @@
 class ItinerariesController < ApplicationController
   def index
-    # @itineraries = Itinerary.all
+    @itineraries = Itinerary.all
   end
 
   def show
-    # @itinerary = Itinerary.find(params[:id])
+    @itinerary = Itinerary.find(params[:id])
+    # @trip = Trip.find(params[:id])
   end
 end
