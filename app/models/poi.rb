683b30d71f2eba427aa15a3840254c152ca950a4
Eric Le Borgne <eric.dev91@gmail.com>
2023-03-07 16:39:14 +0100

    cleaned-seed-still-to-complete

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,11 +1,11 @@
 class Poi < ApplicationRecord
   has_many :itinerary_pois
 
-  Validates :name, presence: true, uniqueness: true
-  Validates :address, presence: true, uniqueness: true
-  Validates :mystery_name, presence: true, uniqueness: true
-  Validates :mystery_content, presence: true
-  Validates :mystery_image, presence: true, uniqueness: true
-  Validates :clue_image, presence: true, uniqueness: true
-  Validates :clue_sentence, presence: true, uniqueness: true
+  # Validates :name, presence: true, uniqueness: true
+  # Validates :address, presence: true, uniqueness: true
+  # Validates :mystery_name, presence: true, uniqueness: true
+  # Validates :mystery_content, presence: true
+  # Validates :mystery_image, presence: true, uniqueness: true
+  # Validates :clue_image, presence: true, uniqueness: true
+  # Validates :clue_sentence, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,38 +11,42 @@ Trip.destroy_all
 Question.destroy_all
 Itinerary.destroy_all
 
+puts 'database cleaned'
 puts '####### Creating Users #############'
 
 User.create!(email: 'test1@test.com', password: 'password', password_confirmation: 'password', nickname: 'Jean-Fanch')
 User.create!(email: 'test2@test.com', password: 'password', password_confirmation: 'password', nickname: 'Jean-Rico')
 User.create!(email: 'test3@test.com', password: 'password', password_confirmation: 'password', nickname: 'Jean-Kevin')
-puts User.count
+puts "created #{User.count} users"
 
 puts '####### Creating Cities #############'
 
-City.create(name: 'Saint-Malo')
-puts City.count
+City.create!(name: 'Saint-Malo')
+City.create!(name: 'Le Mont-Saint-Michel')
+City.create!(name: 'Dinan')
+puts "created #{City.count} cities"
 
 puts '####### Creating Itineraries #############'
 
-Itinerary.create!(name: 'Saint-Malo', distance: 5, rating: 5, duration: 120, number_of_poi: 3, departure: 'Porte Saint-vincent 35400 Saint-Malo', city_id: City.last.id)
-puts Itinerary.count
+Itinerary.create!(name: 'Saint-Malo, la cité corsaire', distance: 5, rating: 5, duration: 120, number_of_poi: 3, departure: 'Porte Saint-vincent 35400 Saint-Malo', city_id: City.last.id)
+Itinerary.create!(name: 'Les secrets du Mont-Saint-Michel', distance: 7, rating: 5, duration: 160, number_of_poi: 4, departure: 'Pont Passerelle du Mont Saint-Michel, 50170 le Mont Saint Michel', city_id: City.last.id)
+Itinerary.create!(name: 'Dinan et ses remparts', distance: 10, rating: 5, duration: 180, number_of_poi: 6, departure: 'Le Vieux Pont 22100 Dinan', city_id: City.last.id)
+puts "created #{Itinerary.count} itineraries"
 
 puts '####### Creating Pois #############'
 
-Poi.create(name: 'Saint-Malo, la cité corsaire', address: 'rue du chat qui danse, 35400 Saint-Malo',
-           mystery_name: 'rue du chat qui danse', mystery_content: "Les vils Anglois ont tenté de rayer Saint-malo de la carte avec un bateau rempli d'explosifs",
-           mystery_image: 'wlecjbcirukxgdhv67wk', clue_image: '?', clue_sentence: 'Je suis un animal domestique, Qui aime les souris et les balles élastiques,
-           Mon nom est court et doux à prononcer, Et quand il y a de la musique, je me mets à bouger, Qui suis-je ?')
-puts Poi.count
-
+Poi.create!(name: 'La rue du chat qui danse', address: '1 rue du chat qui danse, 35400 Saint-Malo',
+            mystery_name: 'rue du chat qui danse', mystery_content: "Les vils Anglois ont tenté de rayer Saint-malo de la carte avec un bateau rempli d'explosifs",
+            mystery_image: 'wlecjbcirukxgdhv67wk', clue_image: '?', clue_sentence: 'Je suis un animal domestique, Qui aime les souris et les balles élastiques,
+            Mon nom est court et doux à prononcer, Et quand il y a de la musique, je me mets à bouger, Qui suis-je ?')
+puts "created #{Poi.count} pois"
 
 puts '####### Creating Trips #############'
 
-Trip.create(proximity_level: 0, content: 'blabla', status: '')
-puts Trip.count
+Trip.create!(proximity_level: 0, content: 'blabla', status: 'running', progress: 'image', user_id: User.last.id, itinerary_id: Itinerary.last.id)
+puts "created #{Trip.count} trips"
 
 puts '####### Creating Questions #############'
 
-puts Question.count
-
+Question.create!(question_statement: 'Quel etait le surnom de la ville de Saint-Malo ? la cité ... :', itinerary_id: Itinerary.last.id)
+puts "created #{Question.count} questions"
