94e7c91bf7c7abc1a05dc16f0a441146de667c42
Eric Le Borgne <eric.dev91@gmail.com>
2023-03-09 21:30:30 +0100

    ajouts de seeds questions answers et dependant destroy sur les modeles question/trip

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :itinerary
-  has_many :trip_answers
+  has_many :trip_answers, dependent: :destroy
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :user
-  has_many :trip_answers
+  has_many :trip_answers, dependent: :destroy
   has_many :questions, through: :trip_answers
   belongs_to :itinerary
   geocoded_by :departure
