eefdb0d2f601e45047a048788c288c75677584ca
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-03-10 13:35:08 +0100

    form ok for the first question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,7 @@
 class QuestionsController < ApplicationController
   def index
     @trip = Trip.find(params[:trip_id])
-    @itinerary = Itinerary.find(params[:id])
     @questions = Question.all
-    @itinerary.questions = @questions
+    @trip.itinerary.questions = @questions
   end
 end
