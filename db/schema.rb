8a2d9e1455cec727dd8c1cd72d52805b3ae1d65a
Eric Le Borgne <eric.dev91@gmail.com>
2023-03-13 15:25:30 +0100

    add subtitle to itineraries et front de itinerary show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_101002) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_131041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_101002) do
     t.float "longitude"
     t.string "category"
     t.text "description"
+    t.string "subtitle"
     t.index ["city_id"], name: "index_itineraries_on_city_id"
   end
 
