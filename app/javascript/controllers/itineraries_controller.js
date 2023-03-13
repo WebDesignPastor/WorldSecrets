3d1c5f49faefba7b106c309a7ee8e551b646d457
JJ Bernard <jeanjulien.bernard@gmail.com>
2023-03-13 12:38:56 +0100

    Fix map issue, need to be reworked after all content has been populated

--- app/javascript/controllers/itineraries_controller.js
+++ app/javascript/controllers/itineraries_controller.js
@@ -12,31 +12,35 @@ export default class extends Controller {
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
-      container: this.element,
+      container: 'itinerary-map',
       zoom:1,
       style: "mapbox://styles/fanchpastor/cleztoc72003801o3sccvya6t"
     })
-
+    
     this.#addMarkersToMap(this.markersValue)
     this.#fitMapToMarkers(this.markersValue)
 
+    this.element.querySelector('canvas').style.height = '100vh'
+    console.log(this.element.querySelector('canvas').style)
+
+    this.map.resize();
   }
 
   #addMarkersToMap(markers) {
-
     markers.forEach((marker) => {
       let itineraryMarker = document.createElement('div');
       itineraryMarker.className = 'itinerary-marker';
       new mapboxgl.Marker(itineraryMarker)
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(this.map)
-
     })
+    
   }
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 5, duration: 0 })
+    this.map.resize();
   }
 }
