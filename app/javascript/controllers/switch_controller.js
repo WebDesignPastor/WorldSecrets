5b1f0538059260726d164a392d53eff30b27538f
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-03-14 17:46:02 +0100

    just a little animation for markers

--- app/javascript/controllers/switch_controller.js
+++ app/javascript/controllers/switch_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="switch"
 export default class extends Controller {
-  static targets = ["list", "map", "svglist", "svgmap", "cardsForMap"]
+  static targets = ["list", "map", "svglist", "svgmap", "cardsForMap", "itinerariesMarker"]
 
   connect() {
   }
@@ -17,6 +17,15 @@ export default class extends Controller {
   }
 
   displayMap() {
+    // recuperer tous les markers en targets
+    // itérer dessus for each
+    this.itinerariesMarkerTargets.forEach((marker, index) => {
+      // leur ajouter une classe animate-marker -> opacity 1
+      marker.classList.add("animate-marker")
+      marker.style.animationDelay = `${index * 200}ms`
+    })
+
+
     this.cardsForMapTarget.classList.remove('d-none')
     this.mapTarget.classList.remove("opacity-0")
     this.listTarget.classList.add("d-none")
