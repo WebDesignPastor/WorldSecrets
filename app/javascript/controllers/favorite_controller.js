8bf2d25268b4849ca09db816099809061b027874
Francois PARMENTIER$ <parmentier.corporate@gmail.com>
2023-03-16 16:28:29 +0100

    Favorite button made

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -2,6 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="favorite"
 export default class extends Controller {
+  static targets = ["button"]
+
   connect() {
+  }
+
+  active(){
+    this.buttonTarget.classList.toggle("button-favorite-active")
   }
 }
