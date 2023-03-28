5703b8bad09fb6f2b441b71383f86db02e6b58d1
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-03-28 14:59:29 +0200

    add a condition to display only the questions which belong to the good itinerary

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,8 @@
 class QuestionsController < ApplicationController
   def index
     @trip = Trip.find(params[:trip_id])
-    @questions = Question.all
+    @questions = Question.where(itinerary_id: @trip.itinerary)
     @trip.itinerary.questions = @questions
-  end
-
-  def update
   end
 
   # def update_rating
