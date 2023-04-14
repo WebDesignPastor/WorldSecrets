7276c20a0cc05a0ad1390a16b0a827696fbcfbf4
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-04-14 09:02:00 +0200

    fix things

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources "itineraries", only: %i[index show create update] do
+  resources "itineraries", only: %i[index show create] do
     resources "trips", only: :create
   end
 
   resources "trips", only: %i[show create update] do
     resources "questions", only: :index do
+      resources "itineraries", only: :update
       member do
         post 'rate/:rating_value', to: 'questions#rate', as: 'rate'
       end
@@ -18,6 +19,4 @@ Rails.application.routes.draw do
   patch "questions/:question_id/itineraries/:id", to: "itineraries#update_rating"
 
   resources "pois", only: :show
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
