6a110abc2db31d9bc0b3512b598e639c73947247
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-04-25 12:49:43 +0200

    Added elements to partial

---
+++ app/javascript/controllers/real_trip_controller.js
@@ -0,0 +1,55 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="real-trip"
+export default class extends Controller {
+
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
+  connect() {
+    console.log("real trip controller");
+
+    mapboxgl.accessToken = this.apiKeyValue;
+
+    navigator.geolocation.watchPosition(position => {
+      const { latitude, longitude } = position.coords;
+      // Show a map centered at latitude / longitude.
+      this.map = new mapboxgl.Map({
+        container: this.element,
+        zoom: 16, // set the initial zoom level
+        center: [longitude, latitude],
+        style: "mapbox://styles/fanchpastor/cleztoc72003801o3sccvya6t",
+      });
+
+      // Add geolocate control to the map.
+      this.map.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          // When active the map will receive updates to the device's location as it changes.
+          trackUserLocation: true,
+          // Draw an arrow next to the location dot to indicate which direction the device is heading.
+          showUserHeading: true,
+        })
+      );
+
+      // Create a marker at the current location and add it to the map.
+      const marker = new mapboxgl.Marker({
+        color: "#007bff", // set the marker color
+      })
+        .setLngLat([longitude, latitude])
+        .addTo(this.map);
+
+      // Zoom the map to the current location.
+      this.map.flyTo({
+        center: [longitude, latitude],
+        zoom: 16, // set the zoom level
+        essential: true,
+      });
+    });
+  }
+
+}
