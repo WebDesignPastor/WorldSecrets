891dfe31cc16923463fb35386425bf29b7c3620c
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-04-27 20:26:07 +0200

    Added latlong to Kerouzere

--- db/seeds.rb
+++ db/seeds.rb
@@ -115,7 +115,7 @@ vannes_itinerary_pictures.each { |file| vannes_itinerary.photos.attach(io: file,
 vannes_itinerary.save!
 
 kerouzere_itinerary_pictures = [File.open("db/fixtures/kerouzere1.png")]
-kerouzere_itinerary = Itinerary.new(name: "Un château figé", subtitle: "Un beau chateau ma foi", distance: 12, category: "Nature", rates: [3], duration: 180, number_of_poi: 4, departure: "Chateau de Kerouzéré, 29250 Sibiril", city: sibiril)
+kerouzere_itinerary = Itinerary.new(name: "Un château figé", subtitle: "Un beau chateau ma foi", distance: 12, category: "Nature", rates: [3], duration: 180, number_of_poi: 4, latitude: 48.672698259366555, longitude: -4.069653969144087, city: sibiril)
 kerouzere_itinerary_pictures.each { |file| kerouzere_itinerary.photos.attach(io: file, filename: "nes.png", content_type: "image/png") }
 kerouzere_itinerary.save!
 
