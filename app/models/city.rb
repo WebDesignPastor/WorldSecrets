507ea0dde5b7fe35e183bf36a5355f2faec527a9
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-05-02 16:28:15 +0200

    Modified models

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,9 @@
 class City < ApplicationRecord
   has_many :itineraries
   validates :name, presence: true, uniqueness: true
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_synopsis, against: [ :name ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
