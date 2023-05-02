e91c80698108e72cbbf6833bdc9b26abdda1829b
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-05-02 16:57:49 +0200

    Edited search bar

--- Gemfile
+++ Gemfile
@@ -50,7 +50,6 @@ gem "sassc-rails"
 gem "geocoder"
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
-gem "pg_search"
 
 gem "devise"
 gem "autoprefixer-rails"
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,9 +1,4 @@
 class City < ApplicationRecord
   has_many :itineraries
   validates :name, presence: true, uniqueness: true
-  include PgSearch::Model
-  pg_search_scope :search_by_title_and_synopsis, against: [ :name ],
-  using: {
-    tsearch: { prefix: true }
-  }
 end
