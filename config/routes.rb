8762069a6bf75e64d4e3b86f978a4b623ea95a92
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-05-16 10:24:26 +0200

    Added route to Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "profile", to: "pages#profile"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources "itineraries", only: %i[index show create update] do
     resources "trips", only: :create
