be77e90f69a39d6b53260b65ec331957585d84a7
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-05-20 19:29:57 +0200

    fixing quizz

--- app/javascript/controllers/questions_controller.js
+++ app/javascript/controllers/questions_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="questions"
 export default class extends Controller {
-  static targets = ["progress", "question", "six"]
+  static targets = ["progress", "question", "congratulation", "badAnswer"]
   currentIndex = -1
   progress = 0
 
@@ -11,8 +11,8 @@ export default class extends Controller {
   }
 
   connect() {
-    this.questionTargets[0].classList.remove('d-none')
-    console.log('je crois que ça y est')
+    this.questionTargets[0].classList.remove('d-none') // to comment if you want to see congratulation first
+    // this.congratulationTarget.classList.remove('d-none') // remove comment if you want to see congratulation first
   }
 
   goodAnswer(event) {
@@ -29,7 +29,7 @@ export default class extends Controller {
         this.questionTargets[this.nextIndex].classList.remove('d-none')
       } else {
         this.questionTargets[this.currentIndex].classList.add('d-none')
-        this.sixTarget.classList.remove('d-none')
+        this.congratulationTarget.classList.remove('d-none')
       }
     }, 2000);
 
@@ -38,140 +38,24 @@ export default class extends Controller {
     this.nextIndex = this.currentIndex + 1
   }
 
-  // goodAnswer1(event) {
-  //   event.currentTarget.classList.remove('btn-outline-primary')
-  //   event.currentTarget.style.backgroundColor = 'green'
-  //   event.currentTarget.style.color = 'white'
-  //   let count = 0
-  //   count += 20
-  //   setTimeout(() => {
-  //     this.progressTarget.classList.add(`progress${count}`)
-  //     this.questionTargets[0].classList.add('d-none')
-  //     this.questionTargets[1].classList.remove('d-none')
-  //   }, 2000);
-  // }
+  badAnswer(event) {
+    event.currentTarget.classList.remove('btn-outline-primary')
+    event.currentTarget.classList.add('btn-primary')
+    event.currentTarget.classList.add('btn-shake')
+    setTimeout(() => {
+      this.badAnswerTargets.forEach(answer => {
+        answer.classList.remove('btn-primary')
+      })
+      this.badAnswerTargets.forEach(answer => {
+        answer.classList.remove('btn-shake')
+      })
+      this.badAnswerTargets.forEach(answer => {
+        answer.classList.add('btn-outline-primary')
+      })
+    }, 2000);
+  }
 
-  // goodAnswer2(event) {
-  //   event.currentTarget.classList.remove('btn-outline-primary')
-  //   event.currentTarget.style.backgroundColor = 'green'
-  //   event.currentTarget.style.color = 'white'
-  //   let count = 0
-  //   count += 40
-  //   setTimeout(() => {
-  //     this.progressTarget.classList.add(`progress${count}`)
-  //     this.questionTargets[1].classList.add('d-none')
-  //     this.questionTargets[2].classList.remove('d-none')
-  //   }, 2000);
-  // }
+  confettis() {
 
-  // goodAnswer3(event) {
-  //   event.currentTarget.classList.remove('btn-outline-primary')
-  //   event.currentTarget.style.backgroundColor = 'green'
-  //   event.currentTarget.style.color = 'white'
-  //   let count = 0
-  //   count += 60
-  //   setTimeout(() => {
-  //     this.progressTarget.classList.add(`progress${count}`)
-  //     this.questionTargets[2].classList.add('d-none')
-  //     this.questionTargets[3].classList.remove('d-none')
-  //   }, 2000);
-  // }
-
-  // goodAnswer4(event) {
-  //   event.currentTarget.classList.remove('btn-outline-primary')
-  //   event.currentTarget.style.backgroundColor = 'green'
-  //   event.currentTarget.style.color = 'white'
-  //   let count = 0
-  //   count += 80
-  //   setTimeout(() => {
-  //     this.progressTarget.classList.add(`progress${count}`)
-  //     this.questionTargets[3].classList.add('d-none')
-  //     this.questionTargets[4].classList.remove('d-none')
-  //   }, 2000);
-  // }
-
-  // goodAnswer5(event) {
-  //   event.currentTarget.classList.remove('btn-outline-primary')
-  //   event.currentTarget.style.backgroundColor = 'green'
-  //   event.currentTarget.style.color = 'white'
-  //   let count = 0
-  //   count += 100
-  //   setTimeout(() => {
-  //     this.progressTarget.classList.add(`progress${count}`)
-  //     this.questionTargets[4].classList.add('d-none')
-  //     this.sixTarget.classList.remove('d-none')
-  //   }, 2000);
-  // }
-
-  // goodanswer1(event) {
-  //   event.currentTarget.classList.remove('btn-outline-primary')
-  //   event.currentTarget.style.backgroundColor = 'green'
-  //   event.currentTarget.style.color = 'white'
-  //   setTimeout(() => {
-  //     this.progressTarget.classList.remove('progress-bar')
-  //     this.progressTarget.classList.add('progress20')
-  //     this.questionTargets[0].classList.add('d-none')
-  //     this.questionTargets[1].classList.remove('d-none')
-  //   }, 2000);
-  // }
-
-  // goodanswer2(event) {
-  //   event.currentTarget.classList.remove('btn-outline-primary')
-  //   event.currentTarget.style.backgroundColor = 'green'
-  //   event.currentTarget.style.color = 'white'
-  //   setTimeout(() => {
-  //     this.progressTarget.classList.remove('progress20')
-  //     this.progressTarget.classList.add('progress40')
-  //     this.questionTargets[1].classList.add('d-none')
-  //     this.questionTargets[2].classList.remove('d-none')
-  //   }, 2000);
-  // }
-
-  // goodanswer3(event) {
-  //   event.currentTarget.classList.remove('btn-outline-primary')
-  //   event.currentTarget.style.backgroundColor = 'green'
-  //   event.currentTarget.style.color = 'white'
-  //   setTimeout(() => {
-  //     this.progressTarget.classList.remove('progress40')
-  //     this.progressTarget.classList.add('progress60')
-  //     this.questionTargets[2].classList.add('d-none')
-  //     this.questionTargets[3].classList.remove('d-none')
-  //   }, 2000);
-  // }
-
-  // goodanswer4(event) {
-  //   event.currentTarget.classList.remove('btn-outline-primary')
-  //   event.currentTarget.style.backgroundColor = 'green'
-  //   event.currentTarget.style.color = 'white'
-  //   setTimeout(() => {
-  //     this.progressTarget.classList.remove('progress60')
-  //     this.progressTarget.classList.add('progress80')
-  //     this.questionTargets[3].classList.add('d-none')
-  //     this.questionTargets[4].classList.remove('d-none')
-  //   }, 2000);
-  // }
-
-  // goodanswer5(event) {
-  //   event.currentTarget.classList.remove('btn-outline-primary')
-  //   event.currentTarget.style.backgroundColor = 'green'
-  //   event.currentTarget.style.color = 'white'
-  //   setTimeout(() => {
-  //     this.progressTarget.classList.remove('progress80')
-  //     this.progressTarget.classList.add('progress100')
-  //     this.questionTargets[4].classList.add('d-none')
-  //     this.sixTarget.classList.remove('d-none')
-  //   }, 2000);
-  // }
-
-  // badanswer(event) {
-  //   const element = event.currentTarget
-  //   element.classList.remove('btn-outline-primary')
-  //   element.classList.add('btn-primary')
-  //   element.classList.add('btn-shake')
-  //   setTimeout(() => {
-  //     element.classList.remove('btn-primary')
-  //     element.classList.add('btn-outline-primary')
-  //     element.classList.remove('btn-shake')
-  //   }, 2000);
-  // }
+  }
 }
