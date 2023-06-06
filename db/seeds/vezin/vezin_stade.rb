b770f068b87bb77d9cd5c3b523f315c0313414f8
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-06-06 15:12:40 +0200

    Refactoring Vezin

--- db/seeds/vezin/vezin_stade.rb
+++ db/seeds/vezin/vezin_stade.rb
@@ -11,8 +11,10 @@ mairie_vezin = Poi.create!(name: 'La mairie de Vezin', latitude: 48.116956035962
 
 
 vezin_itinerary_pictures = [File.open("db/seeds/vezin/images/vezin1.jpg")]
-vezin_itinerary = Itinerary.new(name: 'Vezin et son stade', subtitle: "Le joyau de la Flume", distance: 10, rates: [5], duration: 180, number_of_poi: 2,  latitude: 48.119235, longitude: -1.753901, city: vezin, category: 'Urbain',
-description: "Vezin est le sang de la veine de l'Ille-et-Vilaine. Venez vous installer dans cette bourgade de l'extrême où les générations s'entendent si bien entre elles.")
+vezin_itinerary = Itinerary.new(name: 'Vezin et son stade', subtitle: "Le joyau de la Flume", distance: 10, rates: [5],
+  duration: 180, number_of_poi: 2,  latitude: 48.119235, longitude: -1.753901, city: vezin, category: 'Urbain',
+description: "Vezin est le sang de la veine de l'Ille-et-Vilaine. Venez vous installer dans cette bourgade de l'extrême
+où les générations s'entendent si bien entre elles.")
 vezin_itinerary_pictures.each { |file| vezin_itinerary.photos.attach(io: file, filename: "nes.png", content_type: "image/png") }
 vezin_itinerary.save!
 
@@ -22,10 +24,15 @@ ItineraryPoi.create!(poi_order: 2, itinerary: vezin_itinerary, poi: prison_vezin
 puts "created #{ItineraryPoi.count} ItineraryPois"
 
 
-Question.create!(question_statement: "Quel est le nom de la rue principale de Vezin ?", itinerary: vezin_itinerary, answer_statement: ["Rue de la République", "Rue de la Liberté", "Rue de Rennes", "Rue de la Réunification"], good_answer: "Rue de Rennes")
-Question.create!(question_statement: "Quel est le nom de la prison de Vezin ?", itinerary: vezin_itinerary, answer_statement: ["Prison de Vezin", "Prison de Pacé", "Prison de Rennes", "Prison de Vitré"], good_answer: "Prison de Vezin")
-Question.create!(question_statement: "Quel est le nom de la mairie de Vezin ?", itinerary: vezin_itinerary, answer_statement: ["Mairie de Vezin", "Mairie de Pacé", "Mairie de Rennes", "Mairie de Vitré"], good_answer: "Mairie de Vezin")
-Question.create!(question_statement: "Quel est du supermarché de Vezin ?", itinerary: vezin_itinerary, answer_statement: ["Supermarché de Vezin", "Supermarché de Pacé", "Supermarché de Rennes", "Supermarché de Vitré"], good_answer: "Supermarché de Vezin")
-Question.create!(question_statement: "Quel est du stade de Vezin ?", itinerary: vezin_itinerary, answer_statement: ["Stade de Vezin", "Stade de Pacé", "Stade de Rennes", "Stade de Vitré"], good_answer: "Stade de Vezin")
+Question.create!(question_statement: "Quel est le nom de la rue principale de Vezin ?", itinerary: vezin_itinerary,
+  answer_statement: ["Rue de la République", "Rue de la Liberté", "Rue de Rennes", "Rue de la Réunification"], good_answer: "Rue de Rennes")
+Question.create!(question_statement: "Quel est le nom de la prison de Vezin ?", itinerary: vezin_itinerary,
+  answer_statement: ["Prison de Vezin", "Prison de Pacé", "Prison de Rennes", "Prison de Vitré"], good_answer: "Prison de Vezin")
+Question.create!(question_statement: "Quel est le nom de la mairie de Vezin ?", itinerary: vezin_itinerary,
+  answer_statement: ["Mairie de Vezin", "Mairie de Pacé", "Mairie de Rennes", "Mairie de Vitré"], good_answer: "Mairie de Vezin")
+Question.create!(question_statement: "Quel est du supermarché de Vezin ?", itinerary: vezin_itinerary,
+  answer_statement: ["Supermarché de Vezin", "Supermarché de Pacé", "Supermarché de Rennes", "Supermarché de Vitré"], good_answer: "Supermarché de Vezin")
+Question.create!(question_statement: "Quel est du stade de Vezin ?", itinerary: vezin_itinerary,
+  answer_statement: ["Stade de Vezin", "Stade de Pacé", "Stade de Rennes", "Stade de Vitré"], good_answer: "Stade de Vezin")
 
 puts "vezin_itinerary created"
