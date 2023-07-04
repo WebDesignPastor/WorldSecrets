7d805e43895468944d6ee44addec556309f89bcd
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-07-04 12:02:41 +0200

    all logic for delete account ok

--- app/javascript/controllers/account_controller.js
+++ app/javascript/controllers/account_controller.js
@@ -2,6 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="account"
 export default class extends Controller {
-  connect() {
+  static targets = ["deleteButton", "itinerariesList", "backButton", "itinerariesCategory", "gear", "cross"]
+
+  appearOptions() {
+    this.deleteButtonTarget.classList.remove("d-none")
+    this.backButtonTarget.classList.add("d-none")
+    this.itinerariesListTargets.forEach((list) => {
+      list.classList.add("d-none")
+    })
+    this.itinerariesCategoryTargets.forEach((category) => {
+      category.classList.add("d-none")
+    })
+    this.gearTarget.classList.add("d-none")
+    this.crossTarget.classList.remove("d-none")
+  }
+
+  disappearOptions() {
+    this.deleteButtonTarget.classList.add("d-none")
+    this.backButtonTarget.classList.remove("d-none")
+    this.itinerariesListTargets.forEach((list) => {
+      list.classList.remove("d-none")
+    })
+    this.itinerariesCategoryTargets.forEach((category) => {
+      category.classList.remove("d-none")
+    })
+    this.gearTarget.classList.remove("d-none")
+    this.crossTarget.classList.add("d-none")
   }
 }
