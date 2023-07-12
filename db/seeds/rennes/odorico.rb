5d327f25004c6a6dcd99724fcd0d5dee420c2641
Francois PARMENTIER <parmentier.corporate@gmail.com>
2023-07-12 22:01:40 +0200

    Finished 2nd poi of odorico

--- db/seeds/rennes/odorico.rb
+++ db/seeds/rennes/odorico.rb
@@ -8,8 +8,20 @@ poirier = Poi.create!(
   mystery_content: "Appréciez les magnifiques tesselles dorées. Si l’immeuble à l’architecture typiquement Art déco porte le nom de l’entrepreneur Jean Poirier,
   les mosaïques elles sont bien signées Odorico. Elles datent de 1928 et ont été restaurées en 2016-2017 par la famille Patrizio, originaire du même village que les Odorico",
   mystery_image: 'pois/rennes/poirier.jpg',
-  clue_image: 'pois/rennes/poirier_clue.jpg',
+  clue_image: 'pois/rennes/poirier_clue.png',
   clue_sentence: "Elevez votre regard... ou vous manquerez ce secret",
+)
+
+dupont = Poi.create!(
+  name: 'Place du Champ Jacquet',
+  mystery_name: "31 rue Dupont des Loges",
+  latitude: 48.10920826229957,
+  longitude: -1.6707543920324979,
+  mystery_content: "C’est dans ce quartier que l’activité de la famille Odorico a commencé, comme en témoigne plusieurs décors dans les commerces.
+  Vous pouvez admirer la façade de ce numéro 31, mais n'hésitez pas à flâner dans cette rue pour découvrir d'autres mosaïques.",
+  mystery_image: 'pois/rennes/dupont.jpg',
+  clue_image: 'pois/rennes/dupont_clue.png',
+  clue_sentence: "Cette façade sera du côté impair de la rue",
 )
 
 odorico_itinerary_pictures = [File.open("db/seeds/rennes/images/odorico.png")]
@@ -33,8 +45,8 @@ odorico_itinerary = Itinerary.new(
  odorico_itinerary.save!
 
 ItineraryPoi.create!(poi_order: 1, itinerary: odorico_itinerary, poi: poirier)
-ItineraryPoi.create!(poi_order: 2, itinerary: odorico_itinerary, poi: maison_ti_coz)
-ItineraryPoi.create!(poi_order: 3, itinerary: odorico_itinerary, poi: hotel_noue)
-ItineraryPoi.create!(poi_order: 4, itinerary: odorico_itinerary, poi: rue_chapitre)
+ItineraryPoi.create!(poi_order: 2, itinerary: odorico_itinerary, poi: dupont)
+# ItineraryPoi.create!(poi_order: 3, itinerary: odorico_itinerary, poi: hotel_noue)
+# ItineraryPoi.create!(poi_order: 4, itinerary: odorico_itinerary, poi: rue_chapitre)
 
 puts "created #{ItineraryPoi.count} ItineraryPois"
