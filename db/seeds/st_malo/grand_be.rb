b28be94edbd2dc35d922f502f16faddcfa2582da
François PARMENTIER <webcreationtp@gmail.com>
2024-02-25 04:08:11 +0100

    not much

--- db/seeds/st_malo/grand_be.rb
+++ db/seeds/st_malo/grand_be.rb
@@ -1,7 +1,7 @@
 st_malo = City.create!(name: 'Saint-Malo')
 
 grandbe_itinerary_pictures = [File.open("db/seeds/st_malo/images/GrandBe1.jpg")]
-grandbe_itinerary = Itinerary.new(name: 'La tombe de Chateaubriand', subtitle: "Une promenade entre mer et histoire",
+grandbe_itinerary = Itinerary.new(name: 'La tombe de fameux Chateaubriand', subtitle: "Une promenade entre mer et histoire",
   distance: 4, category: "Nature", rates: [3], duration: 70, number_of_poi: 2, latitude: 48.650861651302534, longitude: -2.0218998515555895, city: st_malo)
 grandbe_itinerary_pictures.each { |file| grandbe_itinerary.photos.attach(io: file, filename: "nes.png", content_type: "image/png") }
 grandbe_itinerary.save!
